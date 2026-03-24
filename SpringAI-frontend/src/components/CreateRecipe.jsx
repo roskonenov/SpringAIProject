a696676850ab0d6235185019969659a961742563
roskonenov <roskonenov@gmail.com>
2026-03-24 20:29:31 +0200

    add AI image generator and recipe creator

--- SpringAI-frontend/src/components/CreateRecipe.jsx
+++ SpringAI-frontend/src/components/CreateRecipe.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useTypingText from '../hooks/useTypingText';
 
 const CreateRecipe = () => {
+  const [ingredients, setIngredients] = useState('');
+  const [cuisine, setCuisine] = useState('');
+  const [dietaryRestrictions, setDietaryRestrictions] = useState('');
+  const [chatResponse, setChatResponse] = useState('');
+
+  const displayText = useTypingText(chatResponse, 25);
+
+  const generateRecipe = async () => {
+    await fetch(`http://localhost:8080/create-recipe?ingredients=${ingredients}&cuisine=${cuisine}&dietaryRestrictions=${dietaryRestrictions}`)
+      .then(res => res.text())
+      .then(data => { setChatResponse(data) })
+      .catch(err => console.error("Error generating response : ", err));
+  }
   return (
-    <div>CreateRecipe</div>
+    <div>
+      <div>
+        <h2>CreateRecipe</h2>
+        <div className='input-container'>
+          <label htmlFor="ingredients">Ingredients</label>
+          <input
+            id='ingredients'
+            type="text"
+            value={ingredients}
+            placeholder='Enter ingredients (comma seperated)'
+            onChange={(e) => setIngredients(e.target.value)}
+          />
+        </div>
+        <div className='input-container'>
+          <label htmlFor="cuisine">Cuisine</label>
+          <input
+            type="text"
+            value={cuisine}
+            placeholder='Enter preferred cuisine'
+            onChange={(e) => setCuisine(e.target.value)}
+          />
+        </div>
+        <div className='input-container'>
+          <label htmlFor="dietaryRestrictions">Dietary restrictions</label>
+          <input
+            type="text"
+            value={dietaryRestrictions}
+            placeholder='Enter dietaryRestrictions'
+            onChange={(e) => setDietaryRestrictions(e.target.value)}
+          />
+        </div>
+      </div>
+      <button className='sec-btn' onClick={generateRecipe}>Make recipe</button>
+      {chatResponse &&
+        <div className='response-container'>
+          <p className='typing'>{displayText}
+            <span className='cursor'>|</span>
+          </p>
+        </div>}
+    </div>
   )
 }
 
--- SpringAI-frontend/src/components/ImageGenerator.jsx
+++ SpringAI-frontend/src/components/ImageGenerator.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { useState } from "react"
 
 const ImageGenerator = () => {
+  const [prompt, setPrompt] = useState('');
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const generateImage = async () => {
+    await fetch(`http://localhost:8080/generate-image?prompt=${prompt}&n=2&height=512&width=512`)
+      .then(res => res.json())
+      .then(data => setImageUrls(data))
+      .catch(err => console.error("Error generating image : ", err))
+  };
   return (
-    <div>ImageGenerator</div>
+    <div>
+      <h2>ImageGenerator</h2>
+      <input
+        type="text"
+        value={prompt}
+        placeholder='Describe the image you want'
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <button
+        className="sec-btn"
+        onClick={generateImage}
+      >
+        Generate Image
+      </button>
+
+      <div className="image-container">
+        {imageUrls.map((url, index) => (
+          <a href={url} key={index} target="_blank">
+            <img  src={url} alt={`Generated ${index}`} />
+          </a>
+        ))}
+        {[...Array(4 - imageUrls.length)].map((_, index) => (
+          <div key={imageUrls.length + index} className="empty-slot"></div>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
